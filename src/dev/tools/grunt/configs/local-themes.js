3f20060cb7489c808d7a6a451a410c1a95f26f68
root <root@DESKTOP-KJT7AT3>
2024-06-03 11:43:16 +0300

    done task 4.1.4(using styles in Magento_Checkout)

---
+++ src/dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,10 @@
+"use strict";
+module.exports = {
+    magentoThema: {
+        area: "frontend",
+        name: "Katsal/magentoThema",
+        locale: "en_US",
+        files: ["css/styles-m", "css/styles-l"],
+        dsl: "less",
+    },
+};
